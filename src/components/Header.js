dd16cd94aec296733f48dbcee98aa6de8c844dfe
Jean Marie Gustave MBONYINSHUTI <giggygustave.gg@gmail.com>
2022-12-19 16:44:38 +0200

    more updates

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+    
+    render() {
+        return (
+            <div className="bg-blue-500 py-4 flex justify-between items-center">
+                <h1 className="text-white font-bold text-xl">Peti ChatBot</h1>
+            </div>
+          );
+    }
+  
+}
+
+
+export default Header;
